728bc47cc2f21e032012fc7763a4ccc6cfc4d3e0
Piyush Sharma <p96sharma@gmail.com>
2017-09-08 09:20:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/layout/SplashScreen.java
@@ -0,0 +1,39 @@
+package com.example.layout;
+
+/**
+ * Created by Piyush Sharma on 07-Sep-17.
+ **/
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(5000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
